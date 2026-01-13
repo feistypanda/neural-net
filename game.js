7b9922f98c060119509f31d8d84be5b2ea8ebfd0
Leo Milberg <milbergleo@gmail.com>
2026-01-13 16:28:04 -0500

    added flexible input length

--- game.js
+++ game.js
@@ -1,7 +1,18 @@
 const doGame = (() =>{
 
-	let record = ""; // record of past games
-	let wins = 0; // num wins
+	function genRandInput (len) {
+
+		let randLetter = () => "rps".split("")[Math.floor(Math.random() * 2.99)];
+		let res = "";
+		for (let i = 0; i < len; i ++) {
+			res += `${randLetter()}-${randLetter()}:`
+		}
+		return res;
+	}
+
+	// start with a non-empty record so that the AI can have a full input
+	let record = genRandInput(8); // record of past games
+	let playerWins = 0; // num playerWins
 	let losses = 0; // num losses
 
 	let newTrainingData = {};
@@ -10,13 +21,13 @@ const doGame = (() =>{
 	function findAiChoice () {
 		const _input = record.slice(0, -1).split(":"); // slice off trailing ":"
 			
-		if (_input.length < 4) {
+		if (_input.length < inputLength) {
 			const ind = Math.floor(Math.random() * 2.99);
 			return {letter: "rps"[ind], ind};
 		};
 
 		// store input for the new data that will be used for training 
-		newTrainingData = copyObj(trainingData.parseData(record.slice(-16, -1))[0]);
+		newTrainingData = copyObj(trainingData.parseData(record.slice(-4 * inputLength, -1))[0]);
 
 		const aiOut = net.run(newTrainingData.input); // use that data to determine the AI's response to the player's PAST actions
 
@@ -25,27 +36,6 @@ const doGame = (() =>{
 		return {letter: "rps".split("")[aiInd], ind: aiInd};
 	}
 
-	function getRandArrElls (arr, numElls) {
-		let res = [];
-		let copy = copyObj(arr);
-		for (let i = 0; i < numElls; i ++) {
-			let ind = Math.floor(Math.random() * (copy.length - 0.01))
-			res.push(copy[ind]);
-			copy.splice(ind, 1);
-		}
-		return res;
-	}
-
-	function retrainAi () {
-
-		allNewTrainingData.push (newTrainingData);
-
-		let usingData = allNewTrainingData.slice(-60);
-		usingData = usingData.concat(getRandArrElls(trainingData.data, usingData.length)); 
-
-		for (let i = 0; i < 10; i ++) net.updateForBatch(usingData, 1);
-	};
-
 	return function (playerInput) {
 		
 		const aiChoice = findAiChoice();
@@ -53,27 +43,25 @@ const doGame = (() =>{
 		let playerInd = {r:0,p:1,s:2}[playerInput];
 		let aiInd = aiChoice.ind;
 
+		// update the game record
+		record += `${playerInput}-${aiChoice.letter}:`;
+
 		let winner = 0; // 0 = ai, 1 = player, 2 = tie
 
 		// get results
 		if (playerInd === (aiInd + 1) % 3) {
 			winner = 1;
-			wins ++;
+			playerWins ++;
 		} else if (playerInd === aiInd) {
 			winner = 2;
 		} else {
 			losses ++;
 		}
-		
-		newTrainingData.expected = (() => {const arr = [0, 0, 0]; arr[playerInd] = 1; return arr})();
 
 		if (newTrainingData.input) {
-			retrainAi();
+			net.retrainSelf (record.slice(0, -1));
 		}
 		
-		// update the game record
-		record += playerInput + "-" + aiChoice.letter + ":";
-
-		return {record, playerWins: wins, aiWins: losses, winner};
+		return {record, playerWins, aiWins: losses, winner};
 	}
 })();
